cae9c60f9290243cd8990a58587962deaf65d3bf
Jacqueline Kong <jek2179@columbia.edu>
2023-09-26 17:02:19 -0700

    Fill out SongTable

--- src/SongsTable.tsx
+++ src/SongsTable.tsx
@@ -9,23 +9,39 @@ import { APISong, Song, apiSongToSong } from "./Models";
 import Table from "./Table";
 
 const columnHelper = createColumnHelper<Song>();
-const columns = [columnHelper.accessor("title", { id: "title" })];
+const columns = [
+  columnHelper.accessor("title", { header: "Song", id: "title" }),
+  columnHelper.accessor("album", { header: "Album", id: "album" }),
+  columnHelper.accessor("artist", { header: "Artist", id: "artist" }),
+  columnHelper.accessor("songLengthSeconds", {
+    header: "Artist",
+    id: "songLengthSeconds",
+  }),
+];
 
 const SongsTable: React.FunctionComponent = () => {
   const [data, setData] = React.useState<Song[]>([]);
 
+  // If we were going to be fetching from multiple endpoints we would probably want
+  // to create a custom hook, but do this for now.
   useEffect(() => {
+    let ignore = false;
     fetch(
       "https://storage.googleapis.com/atticus-frontend-assessment/api/songs.json",
       { method: "GET" }
     )
       .then((res) => res.json())
       .then((apiData: { songs: APISong[] }) => {
-        const apiSongs = apiData.songs;
-        const songs = apiSongs.map((as) => apiSongToSong(as));
-        setData(songs);
+        if (!ignore) {
+          const apiSongs = apiData.songs;
+          const songs = apiSongs.map((as) => apiSongToSong(as));
+          setData(songs);
+        }
       });
-  }, []);
+    return () => {
+      ignore = true;
+    };
+  }, [data]);
 
   return <Table rows={data} columns={columns} />;
 };
