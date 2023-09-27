0a60081c6cb41ae595d034c7a6f0c966ba194d74
Jacqueline Kong <jek2179@columbia.edu>
2023-09-26 17:14:06 -0700

    Add some fetch error handling

--- src/SongsTable.tsx
+++ src/SongsTable.tsx
@@ -1,10 +1,5 @@
 import React, { useEffect } from "react";
-import {
-  useReactTable,
-  getCoreRowModel,
-  createColumnHelper,
-  flexRender,
-} from "@tanstack/react-table";
+import { createColumnHelper } from "@tanstack/react-table";
 import { APISong, Song, apiSongToSong } from "./Models";
 import Table from "./Table";
 
@@ -21,6 +16,7 @@ const columns = [
 
 const SongsTable: React.FunctionComponent = () => {
   const [data, setData] = React.useState<Song[]>([]);
+  const [fetchFailed, setFetchFailed] = React.useState<boolean>(false);
 
   // If we were going to be fetching from multiple endpoints we would probably want
   // to create a custom hook, but do this for now.
@@ -32,18 +28,26 @@ const SongsTable: React.FunctionComponent = () => {
     )
       .then((res) => res.json())
       .then((apiData: { songs: APISong[] }) => {
+        setFetchFailed(false);
         if (!ignore) {
           const apiSongs = apiData.songs;
           const songs = apiSongs.map((as) => apiSongToSong(as));
           setData(songs);
         }
-      });
+      })
+      .catch(() => setFetchFailed(true));
     return () => {
       ignore = true;
     };
-  }, [data]);
+  }, [data, fetchFailed]);
 
-  return <Table rows={data} columns={columns} />;
+  // TODO: Handle empty state and error state; we have the info, just need to display something reasonable
+  return (
+    <React.Fragment>
+      <Table rows={data} columns={columns} />
+      {fetchFailed && <p>ERROR</p>}
+    </React.Fragment>
+  );
 };
 
 export default SongsTable;
